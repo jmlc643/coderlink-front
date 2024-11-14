fba810869f6ce731d6dd6fe9119a75d633d8dae7
jmlc643 <josemarialuyocampos@gmail.com>
2024-11-13 22:37:07 -0500

    update

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,inject,OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { GetAuthorites, GetUserResponse } from '../../../api/auth-api/interfaces';
+import { AuthApiService } from '../../../api/auth-api/auth-api.service';
 
 @Component({
   selector: 'app-header',
@@ -15,20 +17,35 @@ export class HeaderComponent implements OnInit {
   isLoggedIn: boolean = false; // Simula si el usuario está logeado o no
   userName: string = ''; // Almacena el nombre del usuario
   userRole: string = ''; // Almacena el rol del usuario
+  token: string = ""
+  username: GetUserResponse = {
+    username : ''
+  }
+
+  role: GetAuthorites = {
+    authorities: ''
+  }
+
+  authApiService = inject(AuthApiService)
 
   constructor(private router: Router) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     // Detecta la ruta actual y verifica si es la principal
     this.router.events.subscribe(() => {
       this.isPrincipalPage = this.router.url === '/' || this.router.url === '/principal';
     });
+    this.token = this.authApiService.userToken
+    this.loadData()
   }
-  simulateLogin(name: string, role: string) {
-    this.isLoggedIn = true;
-    this.userName = name;
-    this.userRole = role;
+
+  private async loadData(){
+    this.username = await this.authApiService.getUserByToken(this.token)
+    this.userName = this.username.username
+    this.role = await this.authApiService.getAuthoritiesByToken(this.token)
+    this.userRole = this.role.authorities
   }
+
   logout() {
     this.isLoggedIn = false;
     this.userName = '';
